ee47307ebcb05a32026413da6a96d0e3ef80159e
Nagaraj2Ran <87237941+Nagaraj2Ran@users.noreply.github.com>
2023-07-19 14:02:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello world from a Node.js app!')
+    res.send('Hello world from a Node.js app-server1!')
 })
 
 app.listen(3000, () => {
